7eec69d26fbd32bde685d42c99477b5f00f88c4d
RayanAbansir <rayan.abansir@universite-paris-saclay.fr>
2024-03-24 13:18:45 +0100

    INTEGRATION FARMER/CROW, SOUND METHOD TO FIX, GENERATECORNS METHOD ADDED

--- src/Model/Farmer.java
+++ src/Model/Farmer.java
@@ -6,12 +6,13 @@ import java.awt.Point;
 import javax.swing.Timer;
 
 public class Farmer extends MovingUnits {
+    // Farmer attributes
     private boolean isSelected = false; //check if the unit is selected
     private final Timer moveTimer; //used to control the movement of the farmer
-
     private final int scareRange = 16 * 3 * 3; //the range to scare the crow
+    private final int collectingDistance = 16 * 3; //the distance to collect corn
 
-
+    // CONSTRUCTOR
     public Farmer(GameEngine gameEngine) {
         super(new Point(200, 200),gameEngine);
 
@@ -20,7 +21,7 @@ public class Farmer extends MovingUnits {
         moveTimer.start();
     }
 
-    //the method to move the unit to the destination
+    // Move the farmer
     @Override
     public void move() {
         //set a speed for the farmers movement
@@ -53,53 +54,68 @@ public class Farmer extends MovingUnits {
         System.out.println("Farmer moving to " + position);
     }
 
-
+    // Get the position of the farmer
     public Point getPosition() {
         return position;
     }
 
+    // Set the position of the farmer
     public void setPosition(Point position) {
         this.position = position;
     }
 
+    // Get the selected state of the farmer
     public boolean isSelected() {
         return isSelected;
     }
 
+    // Set the selected state of the farmer
     public void setSelected(boolean selected) {
         this.isSelected = selected;
     }
 
+    // Get the destination of the farmer
     public Point getDestination() {
         return destination;
     }
 
+    // Set the destination of the farmer
     public void setDestination(Point destination) {
         this.destination = destination;
     }
 
-    //check if the click is within the circle
+    // Check if the click is within the circle
     public boolean isClickWithinCircle(Point clickPoint, int radius) {
         return clickPoint.distance(position) <= radius;
     }
 
+    // Start the movement of the farmer
     public void startMoveTimer() {
         if (moveTimer != null && !moveTimer.isRunning()) {
             moveTimer.start();
         }
     }
 
+    // Stop the movement of the farmer
+    public void stopMoveTimer() {
+        if (moveTimer != null && moveTimer.isRunning()) {
+            moveTimer.stop();
+        }
+    }
+
+    // Get the scare range of the farmer
     public int getScareRange() {
         return scareRange;
     }
 
+    // Collect corn
     public void collectCorn() {
-
-    }
-
-    public void stopMoveTimer() {
-        if (moveTimer != null && moveTimer.isRunning()) {
-            moveTimer.stop();
+        for (Corn corn : gameEngine.getCorns()) {
+            if (position.distance(corn.getPosition()) <= collectingDistance) {
+                System.out.println("Farmer is collecting corn");
+                gameEngine.removeUnit(corn);
+                System.out.println("Farmer collected corn");
+            }
         }
     }
 }
